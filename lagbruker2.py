95e3eb532b39e6d21158c4b9f8ea686277809e11
Oyewumi <jonathan.oyewumi.a@gmail.com>
2022-12-06 13:51:00 +0100

    Update lagbruker2.py

--- lagbruker2.py
+++ lagbruker2.py
@@ -10,10 +10,31 @@ background=ImageTk.PhotoImage(file='bg.jpg')
 bgLabel=Label(lagbruker_window,image=background)
 bgLabel.grid()
 
-frame=Frame(lagbruker_window,bg='white')
+frame=Frame(lagbruker_window,width=50,height=20,bg='white')
 frame.place(x=554,y=100)
 
-heading=Label(frame,text='OPPRETT EN BRUKER',font=('Microsoft Yahei UI Light',20,'bold'),bg='white',fg='firebrick')
-heading.grid(row=0,column=0,padx=4,pady=2)
+heading=Label(frame,text='Opprett Bruker',font=('Microsoft Yahei UI Light',18,'bold'),bg='white',
+fg='firebrick1')
+heading.place(x=80,y=200)
+heading.grid(row=0,column=0,padx=10,pady=10)
 
-heading.place(x=400,y=200)
+emailBox=Label(frame,text='Email',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+emailBox.grid(row=1,column=0,sticky='w',padx=25)
+
+emailFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white', bg='firebrick1')
+emailFyll.grid(row=2,column=0,sticky='w',padx=25)
+
+BrukernavnBox=Label(frame,text='Brukernavn',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+BrukernavnBox.grid(row=3,column=0,sticky='w',padx=25,pady=(12,0))
+
+BrukernavnFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+BrukernavnFyll.grid(row=4,column=0,sticky='w',padx=25)
+
+
+PassordBox=Label(frame,text='Passord',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+PassordBox.grid(row=5,column=0,sticky='w',padx=25,pady=(12,0))
+
+PassordFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+PassordFyll.grid(row=6,column=0,sticky='w',padx=25)
+
+lagbruker_window.mainloop() #loopen lar programmet kjøre
