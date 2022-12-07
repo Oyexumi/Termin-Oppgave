7aa0dbd019d53b98d3fcecce1868d830e0b24b6d
Oyewumi <jonathan.oyewumi.a@gmail.com>
2022-12-07 17:07:01 +0100

    Signin tar deg nå til login

--- lagbruker2.py
+++ lagbruker2.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from PIL import ImageTk
 
+def login_page():
+    lagbruker_window.destroy()
+    import login2
+
+
 lagbruker_window=Tk()
 lagbruker_window.title('Oprett Bruker') #Applikasjons Tittel
 lagbruker_window.resizable(False,False) #Lar dem ikke endre på applikasjon størrelse
@@ -27,14 +32,17 @@ emailFyll.grid(row=2,column=0,sticky='w',padx=25)
 BrukernavnBox=Label(frame,text='Brukernavn',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
 BrukernavnBox.grid(row=3,column=0,sticky='w',padx=25,pady=(12,0))
 
-BrukernavnFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+BrukernavnFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
 BrukernavnFyll.grid(row=4,column=0,sticky='w',padx=25)
 
 
 PassordBox=Label(frame,text='Passord',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
 PassordBox.grid(row=5,column=0,sticky='w',padx=25,pady=(12,0))
 
-PassordFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+PassordFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
 PassordFyll.grid(row=6,column=0,sticky='w',padx=25)
 
+LagBruker=Button(frame,text='Lag Bruker',bd=0,bg='firebrick1',activebackground='firebrick1',activeforeground='white',
+                    cursor='hand2',font=('Microsoft Yahei UI Light',17,'bold'),width=17,command=login_page)
+LagBruker.grid(row=7,column=0,sticky='w',padx=25,pady=25)
 lagbruker_window.mainloop() #loopen lar programmet kjøre
