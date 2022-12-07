7aab374fbd37f4bbeb4ef3e101f58e616dfb03e2
Oyewumi <jonathan.oyewumi.a@gmail.com>
2022-12-07 22:01:08 +0100

    Update lagbruker2.py

--- lagbruker2.py
+++ lagbruker2.py
@@ -1,11 +1,18 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk
+import pymysql
+
 
 def login_page():
     lagbruker_window.destroy()
     import login2
 
-
+def connect_database():
+    if emailFyll.get()=='' or BrukernavnFyll.get()=='' or PassordFyll.get()=='':
+        messagebox.showerror('Error', 'All feltene må bli fyllt inn')
+    elif PassordFyll.get() != Pass_BK_Fyll.get():
+        messagebox.showerror('Error', 'Passordene matcher ikke')
 lagbruker_window=Tk()
 lagbruker_window.title('Oprett Bruker') #Applikasjons Tittel
 lagbruker_window.resizable(False,False) #Lar dem ikke endre på applikasjon størrelse
@@ -35,14 +42,22 @@ BrukernavnBox.grid(row=3,column=0,sticky='w',padx=25,pady=(12,0))
 BrukernavnFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
 BrukernavnFyll.grid(row=4,column=0,sticky='w',padx=25)
 
-
-PassordBox=Label(frame,text='Passord',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+PassordBox=Label(frame,text='Passord',
+font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
 PassordBox.grid(row=5,column=0,sticky='w',padx=25,pady=(12,0))
 
 PassordFyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
 PassordFyll.grid(row=6,column=0,sticky='w',padx=25)
 
+
+Pass_BK_Box=Label(frame,text='Bekreft Passord',
+font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+Pass_BK_Box.grid(row=7,column=0,sticky='w',padx=25,pady=(12,0))
+
+Pass_BK_Fyll=Entry(frame,width=25,font=('Microsot Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
+Pass_BK_Fyll.grid(row=8,column=0,sticky='w',padx=25)
+
 LagBruker=Button(frame,text='Lag Bruker',bd=0,bg='firebrick1',activebackground='firebrick1',activeforeground='white',
-                    cursor='hand2',font=('Microsoft Yahei UI Light',17,'bold'),width=17,command=login_page)
-LagBruker.grid(row=7,column=0,sticky='w',padx=25,pady=25)
+                    cursor='hand2',font=('Microsoft Yahei UI Light',17,'bold'),width=17,command=connect_database)
+LagBruker.grid(row=9,column=0,sticky='w',padx=25,pady=25)
 lagbruker_window.mainloop() #loopen lar programmet kjøre
